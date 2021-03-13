6c75486ae0bc4a80f556763b343ff33e741787b8
Elteyab Hassan <Elteyab.Hassan@gmail.com>
2021-03-13 12:42:27 +0200

    update da sh**t and fuck this client

--- app/Views/result.php
+++
@@ -1,9 +0,0 @@
-<?= $this->extend('templates/app', esc($number)) ?>
-
-<?= $this->section('content') ?>
-
-<div class="container">
-    <h1>number: <?= esc($number) ?></h1>
-</div>
-
-<?= $this->endSection() ?>
